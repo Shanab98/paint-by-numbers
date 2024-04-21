d20da6bc4b35ee3cf1afd54110379b261e4525ca
Shanab98 <Shanab98@gmail.com>
2024-04-21 19:51:29 -0400

    move files to backend

--- src/user/UserValidationSchema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-export const createUserValidationSchema = Joi.object({
-  username: Joi.string().required(),
-  password: Joi.string().required(),
-  email: Joi.string().required(),
-});
